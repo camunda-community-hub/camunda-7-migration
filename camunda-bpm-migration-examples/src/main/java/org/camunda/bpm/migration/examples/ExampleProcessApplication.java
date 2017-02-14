44b61f1da6acb7273d76fc747f59671094e43fef
Malte Sörensen <malte.soerensen@holisticon.de>
2017-02-14 14:08:20 +0100

    feat: #3 upgrade camunda-spring-boot-starter to 2.0.0, spring-boot to 1.4.2, camunda-bpm to 7.6.0

--- camunda-bpm-migration-examples/src/main/java/org/camunda/bpm/migration/examples/ExampleProcessApplication.java
+++ camunda-bpm-migration-examples/src/main/java/org/camunda/bpm/migration/examples/ExampleProcessApplication.java
@@ -1,12 +1,10 @@
 package org.camunda.bpm.migration.examples;
 
-import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.spring.boot.starter.SpringBootProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ProcessApplication
 public class ExampleProcessApplication extends SpringBootProcessApplication {
 
 	public static void main(String... args) {
