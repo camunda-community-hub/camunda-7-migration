1f5600e7dfc2f14ffcf102678b49e7d0d8b2c771
Ruwen Reddig <ruwen.reddig@gmail.com>
2017-05-22 19:41:03 +0200

    dynamic port in 'startProcess' link

--- camunda-bpm-migration-examples/src/main/java/org/camunda/bpm/extension/migration/examples/StartProcessListener.java
+++ camunda-bpm-migration-examples/src/main/java/org/camunda/bpm/extension/migration/examples/StartProcessListener.java
@@ -4,6 +4,7 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +17,16 @@ public class StartProcessListener {
   @Autowired
   private RuntimeService runtimeService;
 
+  @Value("${server.port:8080}")
+  private String port;
+
   @GetMapping("/startProcess")
   public String start() {
     log.info("Starting process main-v1");
     ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("main", "Nr. 123", Variables.putValue("FormField_19huq07", "Nr. 123"));
-    return "<a href=\"http://localhost:8080/app/cockpit/default/#/process-instance/"
+    return "<a href=\"http://localhost:" +
+      port +
+      "/app/cockpit/default/#/process-instance/"
       + processInstance.getId()
       + "?viewbox=%7B%22Definitions_1%22%3A%7B%22x%22%3A-248%2C%22y%22%3A-30%2C%22width%22%3A1342%2C%22height%22%3A430%7D%7D\">click</a>";
   }
